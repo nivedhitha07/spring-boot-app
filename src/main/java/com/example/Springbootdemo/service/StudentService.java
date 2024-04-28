3b1c78aac41199406a9db783c34036c1e2df3f5b
nivedhitha <nivedhitha999@gmail.com>
2024-04-28 23:00:49 +0530

    third commit

--- src/main/java/com/example/Springbootdemo/service/StudentService.java
+++ src/main/java/com/example/Springbootdemo/service/StudentService.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
-    public List<Student> findAllStudents(){
-        return List.of(
-                new Student("Nivi","Sriniv",LocalDate.now(),"nivi@gmail.com",13),
-                new Student("Raji","Sriniv", LocalDate.now(),"nivi@gmail.com",13)
-        );
-    }
+
+public interface StudentService {
+     public List<Student> findAllStudents();
+
+
 
 }
