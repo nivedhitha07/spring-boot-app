728aa9e639d0df64fc9228f09ae08da3f8a50c86
nivedhitha <nivedhitha999@gmail.com>
2024-04-28 20:13:16 +0530

    first commit of springboot project

---
+++ src/main/java/com/example/Springbootdemo/SpringbootdemoRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdemoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootdemoRestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/Springbootdemo/controller/StudentController.java
@@ -0,0 +1,16 @@
+package com.example.Springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+    @GetMapping
+    public List<String> findAllStudents(){
+        return List.of("Nivedhitha","java-spring boot developer","walmart");
+    }
+}
